a1a4919566783aa3b3fc4e8b436f6ffec79894a9
Akash R Chandran <chandranrakash@gmail.com>
2024-05-18 19:20:38 +0530

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: "TinyShare",
+  base: "/TinyShare/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
