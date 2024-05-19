e46db925270106becbaee529da2ce3a31b1bd84b
Akash R Chandran <chandranrakash@gmail.com>
2024-05-20 03:12:47 +0530

    added file upload option

---
+++ src/components/DialogUploader.tsx
@@ -0,0 +1,39 @@
+import * as React from "react"
+
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { FileUploader } from "@/components/FileUploader"
+
+export function DialogUploaderDemo() {
+  const [files, setFiles] = React.useState<File[]>([])
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">
+          Upload files {files.length > 0 && `(${files.length})`}
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-xl">
+        <DialogHeader>
+          <DialogTitle>Upload files</DialogTitle>
+          <DialogDescription>
+            Drag and drop your files here or click to browse.
+          </DialogDescription>
+        </DialogHeader>
+        <FileUploader
+          maxFiles={8}
+          maxSize={5 * 1024 * 1024}
+          onValueChange={setFiles}
+        />
+      </DialogContent>
+    </Dialog>
+  )
+}
